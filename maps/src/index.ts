c5a5a42b4c2e619a8c1e3c56438a0e708f767338
Mo <mohamedjama53@gmail.com>
2020-01-11 16:31:07 +0000

    adding type defintions

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,17 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
 
-google;
+new google.maps.Map(document.getElementById('map'), {
+  backgroundColor: 'blue',
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
