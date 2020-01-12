75efb72696f1a5b28b44a12df57a7085006de943
Mo <mohamedjama53@gmail.com>
2020-01-12 08:55:23 +0000

    add optional implements clause

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,7 +1,7 @@
 /* Instructions to every other class on
 how they can be an argument to `addMarker`
 */
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
