8bbc9e242eeb5285692bcef3989bf6a8c3ae7dd3
Mo <mohamedjama53@gmail.com>
2020-01-18 16:50:46 +0000

    add NumbersCollection.ts file

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,32 +1,4 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                // ALl of this only works if collection is number[]
-                //If collection is array of numbers
-                if (this.collection instanceof Array) {
-                    //collection === number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                //Only going to work if collection is a string
-                //If collection is a string, do this logic instead:
-                //~~logic to compare and swap characters in a string
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
+// const sorter = new Sorter([10, 3, -5, 0]);
+// sorter.sort();
+// console.log(sorter.collection);
