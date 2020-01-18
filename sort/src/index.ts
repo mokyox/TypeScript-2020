44a9a56a6498cd2225dd5c67815d78d8a0857d51
Mo <mohamedjama53@gmail.com>
2020-01-18 17:55:46 +0000

    add characterCollection to index.ts to test

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 20]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaaaabya');
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
+console.log(charactersCollection.data);
