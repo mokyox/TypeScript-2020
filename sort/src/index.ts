d9e5f1a6d54c43c3b6e574e1132471b6032df901
Mo <mohamedjama53@gmail.com>
2020-01-18 17:40:02 +0000

    add Inteface Sortable to Sorter

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 10000]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 20]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
