5bb6c34716debc00f56f61fb25bfee4e3639de98
Mo <mohamedjama53@gmail.com>
2020-01-19 18:10:03 +0000

    add LinkedList implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 20]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('Xaaaabya');
 const characterSorter = new Sorter(charactersCollection);
 characterSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
