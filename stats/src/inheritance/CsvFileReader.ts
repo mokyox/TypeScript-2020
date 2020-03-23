1a47d8d5a890d93c3eb08bb5870e70b8d8fd402d
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-03-23 14:15:30 +0000

    comment out inheritance files

--- stats/src/inheritance/CsvFileReader.ts
+++ stats/src/inheritance/CsvFileReader.ts
@@ -1,21 +1,21 @@
-import fs from 'fs';
+// import fs from 'fs';
 
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+// export abstract class CsvFileReader<T> {
+//   data: T[] = [];
 
-  constructor(public filename: string) {}
+//   constructor(public filename: string) {}
 
-  abstract mapRow(row: string[]): T;
+//   abstract mapRow(row: string[]): T;
 
-  read(): void {
-    this.data = fs
-      .readFileSync(this.filename, {
-        encoding: 'utf-8'
-      })
-      .split('\n')
-      .map((row: string): string[] => {
-        return row.split(',');
-      })
-      .map(this.mapRow);
-  }
-}
+//   read(): void {
+//     this.data = fs
+//       .readFileSync(this.filename, {
+//         encoding: 'utf-8'
+//       })
+//       .split('\n')
+//       .map((row: string): string[] => {
+//         return row.split(',');
+//       })
+//       .map(this.mapRow);
+//   }
+// }
